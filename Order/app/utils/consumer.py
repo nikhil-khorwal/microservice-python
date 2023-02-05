676eab70154803b755a0b67ae1fe466a85d902f5
Nikhil Khorwal <nikkhorwal@gmail.com>
2023-02-05 23:41:19 +0530

    docker changes

--- Order/app/utils/consumer.py
+++ Order/app/utils/consumer.py
@@ -1,13 +1,10 @@
 from kafka import KafkaConsumer
 import json
 
-class OrderConsumer:
-    def __init__(self) -> None:
-        self.consumer = KafkaConsumer('order_kafka',
+consumer = KafkaConsumer('order_kafka',
             bootstrap_servers='localhost:9092',
             auto_offset_reset='earliest',
             value_deserializer = json.loads)
 
-    def read(self):
-        for msg in self.consumer:
-            print(msg.value)
+for msg in consumer:
+    print(msg.value)
